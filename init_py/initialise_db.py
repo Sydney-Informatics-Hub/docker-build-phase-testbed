212279a64d0ac8c00d73c8b93951890a6b6cfbc8
Mike Lynch <m.lynch@sydney.edu.au>
2026-04-23 15:55:30 +1000

    Adds 1000 random rows to the database

--- init_py/initialise_db.py
+++ init_py/initialise_db.py
@@ -1,15 +1,25 @@
 import mariadb
 import os
+import random
 import sys
+
+def rword():
+    return ''.join([random.choice('sdnufdfsdier') for i in range(32)])
 
 def main(db_config):
     conn = None
     cursor = None
     try:
-        print("Connecting to MariaDB...")
-        conn = mariadb.connect(**db_config)
-        print("Connection successful!")
-        cursor = conn.cursor()
+        with mariadb.connect(**db_config) as conn:
+            with conn.cursor() as cursor:
+                sql = "CREATE TABLE mydata ( name CHAR(32), count INT )"
+                cursor.execute(sql)
+                conn.commit()
+                sql = "INSERT INTO mydata (name, count) VALUES (?,?)"
+                for k in range(1000):
+                    data = (rword(), k)
+                    cursor.execute(sql, data)
+                conn.commit()
     except Exception as e:
         print("Connection failed")
         print(e)
